657c9fe225ead9df05ec7807033f5b41b7c66687
romariomirandadias <rmirandadias@hotmail.com>
2020-10-16 23:21:44 -0300

    Tratando Exceções e limpando a tela

--- src/aplicattion/ProgramXadrez.java
+++ src/aplicattion/ProgramXadrez.java
@@ -1,10 +1,12 @@
 package aplicattion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import jogo_xadrez.PartidaXadrez;
 import jogo_xadrez.PecaXadrez;
 import jogo_xadrez.PosicaoXadrez;
+import jogo_xadrez.XadrezExcecao;
 
 public class ProgramXadrez {
 
@@ -15,6 +17,8 @@ public class ProgramXadrez {
 		UI.imprimirTabuleiro(partidaXadrez.getPecas());
 
 		while (true) {
+			try {
+				UI.limparTela();
 			UI.imprimirTabuleiro(partidaXadrez.getPecas());
 			System.out.println();
 			System.out.print("Origem: ");
@@ -26,6 +30,15 @@ public class ProgramXadrez {
 
 			PecaXadrez capturarPeca = partidaXadrez.realizarMovimentoXadrez(origem, destino);
 			System.out.println(capturarPeca);
+			}
+			catch (XadrezExcecao e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 	}
 		
 }
